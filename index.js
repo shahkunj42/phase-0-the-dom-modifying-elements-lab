c65da97a5a7adfe3a0b41881a0683a14dc1bca5b
Kunj <shahkunj42@gmail.com>
2022-06-07 17:52:35 -0400

    commit tree 1375a54acef71bd641c3164670e2739e18c555cd

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const body = document.querySelector(body)
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id","victory");
+newHeader.textContent = 'YOUR-NAME is the champion'
+document.body.append(newHeader)
